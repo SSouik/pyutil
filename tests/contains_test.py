d7a0df66fba0804fbc8f106c92976f0606a47591
SSouik <me@samuelsouik.com>
2022-01-03 23:03:55 -0600

    docs: updated README and fixed linting in a test file

--- tests/contains_test.py
+++ tests/contains_test.py
@@ -3,45 +3,55 @@ from pyutil.contains import contains
 
 
 def test_contains_with_empty_list():
-    actual = contains([], 'foo')
+    actual = contains([], "foo")
     assert actual == False
+
 
 def test_contains_with_empty_tuple():
-    actual = contains((), 'foo')
+    actual = contains((), "foo")
     assert actual == False
+
 
 def test_contains_with_empty_dict():
-    actual = contains({}, 'foo')
+    actual = contains({}, "foo")
     assert actual == False
 
+
 def test_contains_with_empty_string():
-    actual = contains('', 'foo')
+    actual = contains("", "foo")
     assert actual == False
+
 
 def test_contains_with_int():
     with pytest.raises(TypeError):
-        contains(1, 'foo')
+        contains(1, "foo")
+
 
 def test_contains_with_bool():
     with pytest.raises(TypeError):
-        contains(True, 'foo')
+        contains(True, "foo")
+
 
 def test_contains_with_callable():
     with pytest.raises(TypeError):
-        contains(lambda: 1, 'foo')
+        contains(lambda: 1, "foo")
+
 
 def test_contains_when_list_has_value():
     actual = contains([1, 2, 3], 2)
     assert actual == True
 
+
 def test_contains_when_set_has_value():
     actual = contains({1, 2, 3}, 2)
     assert actual == True
 
+
 def test_contains_when_dict_has_value_as_key():
-    actual = contains({'a': 1, 'b': 2}, 'a')
+    actual = contains({"a": 1, "b": 2}, "a")
     assert actual == True
 
+
 def test_contains_when_dict_has_value_as_value():
-    actual = contains({'a': 1, 'b': 2}, 1)
+    actual = contains({"a": 1, "b": 2}, 1)
     assert actual == True
