7cdfac6f663675b21878bcfdcaa34df800c07aca
Sam Souik <me@samuelsouik.com>
2022-01-27 22:43:10 -0600

    ci(release): created release script

--- bin/version_check.sh
+++ bin/version_check.sh
@@ -1,10 +1,13 @@
 upstream_branch="https://raw.githubusercontent.com/SSouik/pyutil/main/pyutil/__init__.py"
-upstream_version=$(curl $upstream_branch | grep -m1 __version__ pyutil/__init__.py | sed 's/[".]//g')
+upstream_version=$(curl $upstream_branch | cut -d ' ' -f 3 <<< $(grep -m1 __version__) | sed 's/[".]//g')
 
-current_version=$(grep -m1 __version__ pyutil/__init__.py | sed 's/[".]//g')
+current_version=$(cut -d ' ' -f 3 <<< $(grep -m1 __version__ pyutil/__init__.py) | sed 's/[".]//g')
+
+echo $upstream_version
+echo $current_version
 
 if [[ $upstream_version -ge $current_version ]]; then
-    echo "Current version is not valid. Please bump the minor or patch version"
+    echo "Current version is not valid. Please bump the minor or patch version."
     exit 1
 fi
 
